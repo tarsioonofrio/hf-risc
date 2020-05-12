c6f0a7a6397c9f2f538fbbb542b72b281c5c84e9
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-11 22:22:51 -0300

    My first commit in work. Setup environment to work in local pc and in remote server.

---
+++ hf-riscv-sim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rsync -avz -e 'ssh' ./tools user@192.168.15.210:/home/user/tp
+rsync -avz -e 'ssh' ./tp1 user@192.168.15.210:/home/user/tp
+ssh -tt user@192.168.15.210 << EOF
+    PATH=$PATH:/usr/local/mips-elf/gcc/bin:/usr/local/riscv32-unknown-elf/gcc/bin
+    make clean      -C /home/user/tp/tp1
+    make ${1:-main} -C /home/user/tp/tp1
+    /home/user/tp/tools/sim/hf_riscv_sim/hf_riscv_sim /home/user/tp/tp1/code.bin
+EOF
