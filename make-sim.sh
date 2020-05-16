630d2097c130231891b833f23266a7bb865f9407
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-15 23:31:32 -0300

    Add gitignore and scripts to compile locally

--- make-sim.sh
+++ make-sim.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-ssh user@192.168.15.210 << EOF
-    make clean -C /home/user/tp/tools/sim/hf_riscv_sim
-    make -C /home/user/tp/tools/sim/hf_riscv_sim
-EOF
+make clean -C .tools/sim/hf_riscv_sim
+make -C ./tools/sim/hf_riscv_sim
