4e3d3a3e34ed3d2a79e476ac67727ecda9cd7d49
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-15 23:11:47 -0300

    add more examples and create compilation database

--- tp1/app/main.c
+++ tp1/app/main.c
@@ -4,11 +4,15 @@
 #if defined __riscv
     #include <hf-risc.h>
 #else
-    #include <stdint.h>
     #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdint.h>
     #include <setjmp.h>
+    #include <unistd.h>
     #define delay_ms(x) sleep(x/1000)
 #endif
+#include "list.h"
+
 
 #define DELAY		1000
 #define N_TASKS		4
@@ -18,7 +22,6 @@
     int32_t setjmp(jmp_buf env);
     void longjmp(jmp_buf env, int32_t val);
 #endif
-
 
 static jmp_buf jmp[N_TASKS];
 static int cur;
@@ -96,9 +99,54 @@ void task0(void)
 	}
 }
 
+
+void task(int id_task)
+{
+    volatile char cushion[1000];	/* reserve some stack space */
+    cushion[0] = '@';		/* don't optimize my cushion away */
+
+    if (!setjmp(jmp[id_task]))
+        task1();
+
+    while (1) {			/* thread body */
+        context_switch();
+
+        printf("task 0...\n");
+        delay_ms(DELAY);
+    }
+}
+
+
+#if defined __riscv
+    char mem_pool[8192];
+#endif
+
+
 int main(void)
 {
-	task0();
+    TIMER1PRE = TIMERPRE_DIV256;
+
+    /* TIMER1 base frequency: 5M cycles */
+    /* 5000000 / 256 = 19531.25 */
+    TIMER1CTC = 19531;
+
+    /* TIMER1 alt frequency: 100k cycles */
+    /* 100000 / 256 = 390.625 */
+    TIMER1OCR = 391;
+
+    /* unlock TIMER1 for reset */
+    TIMER1 = TIMERSET;
+    TIMER1 = 0;
+
+    /* enable interrupt mask for TIMER1 CTC and OCR events */
+    TIMERMASK |= MASK_TIMER1CTC | MASK_TIMER1OCR;
+
+    for(;;){
+    }
+
+
+
+//    task0();
 
 	return 0;
 }
