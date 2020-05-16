66f1b8578cd32ac88f636412d5e4ad4f82fc5dc1
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-16 20:47:34 -0300

    Add function to list

--- hf-riscv-sim-remote.sh
+++ hf-riscv-sim-remote.sh
@@ -4,6 +4,6 @@ rsync -avz -e 'ssh' ./tp1 user@192.168.15.210:/home/user/tp
 ssh -tt user@192.168.15.210 << EOF
     PATH=$PATH:/usr/local/mips-elf/gcc/bin:/usr/local/riscv32-unknown-elf/gcc/bin
     make clean      -C /home/user/tp/tp1
-    make ${1:-main} -C /home/user/tp/tp1
+    make ${1:-scheduler} -C /home/user/tp/tp1
     /home/user/tp/tools/sim/hf_riscv_sim/hf_riscv_sim /home/user/tp/tp1/code.bin
 EOF
