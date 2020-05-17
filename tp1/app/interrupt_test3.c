6e3bc8d5965126ee0bc579d947ff028aa1c3197c
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-16 22:11:38 -0300

    I added a comment with an important question

--- tp1/app/interrupt_test3.c
+++ tp1/app/interrupt_test3.c
@@ -3,16 +3,19 @@
 void timer1ctc_handler(void)
 {
 	printf("TIMER1CTC %d\n", TIMER0);
+    delay_ms(1000);
+    printf("END TIMER1CTC %d\n", TIMER0);
+
 }
 
 void timer1ocr_handler(void)
 {
 	static uint32_t tmr1ocr = 0;
-	
+
 	if (++tmr1ocr & 1) {
 		printf("TIMER1OCR %d\n", TIMER0);
 	}
-	
+
 	/* schedule next interrupt (half period) */
 	TIMER1OCR = (TIMER1OCR + 196) % 19531;
 }
@@ -23,8 +26,9 @@ int main(void){
 	/* TIMER1 base frequency: 5M cycles */
 	/* 5000000 / 256 = 19531.25 */
 	TIMER1CTC = 19531;
-	
-	/* TIMER1 alt frequency: 100k cycles */
+//    TIMER1CTC = 32768;
+
+    /* TIMER1 alt frequency: 100k cycles */
 	/* 100000 / 256 = 390.625 */
 	TIMER1OCR = 391;
 
@@ -34,7 +38,9 @@ int main(void){
 
 	/* enable interrupt mask for TIMER1 CTC and OCR events */
 	TIMERMASK |= MASK_TIMER1CTC | MASK_TIMER1OCR;
-
+    long int i;
 	for(;;){
-	}
+        printf("print %ld\n", i++);
+        delay_ms(10);
+    }
 }
