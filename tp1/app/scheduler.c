846ecb4fd73e4b142219a2e2a923d85be8793544
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-05-17 20:21:37 -0300

    Start working on rate monotonic scheduler.

--- tp1/app/scheduler.c
+++ tp1/app/scheduler.c
@@ -34,7 +34,7 @@
 
 
 #define DELAY		1000
-#define N_TASKS		4
+#define N_TASKS		3
 
 #if defined __riscv
     typedef uint32_t jmp_buf[20];
@@ -50,24 +50,79 @@ static int rt_curr;
     char mem_pool[8192];
 #endif
 
+// reserve some stack space
+#define RT_RESERVE_SPACE volatile char cushion[1000]
+// don't optimize my cushion away
+#define RT_DONT_OPTIMIZE  cushion[0] = '@'
+
+
 void task0();
 void task1();
 void task2();
 void rt_task();
 
+typedef struct{
+    int period;
+    int execution;
+    int deadline;
+} rt_parameter;
+
+enum enum_state{READY, RUNNING, BLOCKED} rt_state;
+
+typedef struct{
+    int period;
+    int execution;
+    int deadline;
+} rt_control;
+
+
 struct list *rt_list_function;
+struct list *rt_list_parameter;
+struct list *rt_list_state;
 
+int rt_vec_period[3];
+int rt_vec_execution[3];
+int rt_vec_deadline[3];
+int rt_vec_state[3];
 
-
-
-void context_switch()
+void rt_context_switch_circular()
 {
 	if (!setjmp(rt_jmp[rt_curr])) {
-		if (list_count(rt_list_function) == ++rt_curr)
+        rt_curr++;
+		if (list_count(rt_list_function) == rt_curr)
             rt_curr = 0;
 		longjmp(rt_jmp[rt_curr], 1);
 	}
 }
+
+void rt_context_switch()
+{
+    int index=0, value=0, count=0;
+    int best_index=-1, best_value=65535;
+    count = list_count(rt_list_function);
+
+    if (!setjmp(rt_jmp[rt_curr])) {
+        best_index = rt_curr;
+        best_value = rt_vec_period[rt_curr];
+
+        for (index=0; index < count; index++) {
+            if (rt_vec_state[index] != READY)
+                continue;
+            if (rt_vec_period[index] < best_value) {
+                best_value = rt_vec_period[index];
+                best_index = index;
+            }
+        }
+        rt_vec_state[best_index] = RUNNING;
+
+        if (best_index == rt_curr)
+            rt_vec_state[rt_curr] = BLOCKED;
+
+        rt_curr = best_index;
+        longjmp(rt_jmp[rt_curr], 1);
+    }
+}
+
 
 void rt_task()
 {
@@ -78,12 +133,34 @@ void rt_task()
 	setjmp(rt_jmp[rt_curr]);
 
 	while (1) {			/* thread body */
-		context_switch();
-
+        rt_context_switch();
 		printf("rt task...%d\n", rt_curr);
 		delay_ms(DELAY);
     }
 }
+
+
+void rt_task_test(void)
+{
+    volatile char cushion[1000];	/* reserve some stack space */
+    cushion[0] = '@';		/* don't optimize my cushion away */
+
+    void (*func)();
+    if (!setjmp(rt_jmp[rt_curr])) {
+        if (rt_curr < list_count(rt_list_function) -1) {
+            rt_curr++;
+            func = list_get(rt_list_function, rt_curr);
+            func();
+        }
+    }
+
+    while (1) {			/* thread body */
+        rt_context_switch();
+        printf("rt_task_test id: %d\n", rt_curr);
+        delay_ms(DELAY);
+    }
+}
+
 
 void task2(void)
 {
@@ -99,9 +176,9 @@ void task2(void)
     }
 
 	while (1) {			/* thread body */
-		context_switch();
+        rt_context_switch();
 
-		printf("task 2...%d\n", rt_curr);
+		printf("\rtask 2...%d\n", rt_curr);
 		delay_ms(DELAY);
     }
 }
@@ -109,17 +186,20 @@ void task2(void)
 void task1(void) {
     volatile char cushion[1000];    /* reserve some stack space */
     cushion[0] = '@';        /* don't optimize my cushion away */
+
     void (*func)();
     if (!setjmp(rt_jmp[rt_curr])) {
-        rt_curr++;
-        func = list_get(rt_list_function, rt_curr);
-        func();
+        if (rt_curr < list_count(rt_list_function) -1) {
+            rt_curr++;
+            func = list_get(rt_list_function, rt_curr);
+            func();
+        }
     }
 
 	while (1) {			/* thread body */
-		context_switch();
+        rt_context_switch();
 
-		printf("task 1...%d\n", rt_curr);
+		printf("\rtask 1...%d\n", rt_curr);
 		delay_ms(DELAY);
     }
 }
@@ -128,20 +208,24 @@ void task0(void)
 {
 	volatile char cushion[1000];	/* reserve some stack space */
 	cushion[0] = '@';		/* don't optimize my cushion away */
+
     void (*func)();
-	if (!setjmp(rt_jmp[rt_curr])) {
-        rt_curr++;
-        func = list_get(rt_list_function, rt_curr);
-        func();
+    if (!setjmp(rt_jmp[rt_curr])) {
+        if (rt_curr < list_count(rt_list_function) -1) {
+            rt_curr++;
+            func = list_get(rt_list_function, rt_curr);
+            func();
+        }
     }
 
 	while (1) {			/* thread body */
-		context_switch();
+        rt_context_switch();
 
-		printf("task 0...%d\n", rt_curr);
+		printf("\rtask 0...%d\n", rt_curr);
 		delay_ms(DELAY);
     }
 }
+
 
 #if defined __riscv
     void timer1ctc_handler(void)
@@ -189,14 +273,24 @@ int main(void)
     //    for(;;);
         heap_init((uint32_t *)&mem_pool, sizeof(mem_pool));
     #endif
-
+//    RT_VAR;
     void (*func)();
 
     rt_list_function = list_init();
+    rt_list_parameter = list_init();
     if(list_append(rt_list_function, task0)) printf("FAIL!");
+    rt_vec_period[0] = 4;
+    rt_vec_execution[0] = 1;
+    rt_vec_state[0] = READY;
     if(list_append(rt_list_function, task1)) printf("FAIL!");
+    rt_vec_period[1] = 5;
+    rt_vec_execution[1] = 2;
+    rt_vec_state[1] = READY;
     if(list_append(rt_list_function, task2)) printf("FAIL!");
-//    if(list_append(rt_list_function, rt_task)) printf("FAIL!");
+    rt_vec_period[2] = 7;
+    rt_vec_execution[2] = 2;
+    rt_vec_state[2] = READY;
+//    if(list_append(rt_list_function, rt_task_test)) printf("FAIL!");
 
     func = list_get(rt_list_function, 0);
 
